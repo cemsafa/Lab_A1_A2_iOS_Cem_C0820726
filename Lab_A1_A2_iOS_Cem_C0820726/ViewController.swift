f419c2b2d95b1e5a682d250bc63c1a43f4c0503f
Cem Safa <c0820726@mylambton.ca>
2021-09-17 19:47:19 -0400

    initial commit

---
+++ Lab_A1_A2_iOS_Cem_C0820726/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Cem_C0820726
+//
+//  Created by Cem Safa on 2021-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
